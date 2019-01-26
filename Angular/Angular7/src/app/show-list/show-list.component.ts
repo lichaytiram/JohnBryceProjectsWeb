bf3fbd02f0f25b68cc918168d4f3ef915f24a800
lichay <lichaytiram@gmail.com>
2019-01-26 22:57:29 +0200

    web

--- Angular/Angular7/src/app/show-list/show-list.component.ts
+++ Angular/Angular7/src/app/show-list/show-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { todo } from '../shared/models/todo.model';
 
 @Component({
   selector: 'app-show-list',
@@ -7,5 +8,49 @@ import { Component } from '@angular/core';
 })
 export class ShowListComponent {
 
-  public selectby: string;
+  public select_by: string = "date";
+  public instance: todo[];
+  public chosen: todo[];
+
+  constructor() {
+    this.instance = JSON.parse(localStorage.getItem("todo_key"));
+  }
+
+  public change_it() {
+    if (this.select_by == "date") {
+      this.chosen = this.instance.sort(
+        // (a, b) => { return <any>a.date.getMonth - <any>b.date.getMonth }
+        // (a, b) => parseFloat(<any>a.date.getMonth) - parseFloat(<any>b.date.getMonth)
+        function compare(a, b) {
+          if (a.date.getTime < b.date.getTime)
+            return -1;
+          if (a.date.getTime > b.date.getTime)
+            return 1;
+          return 0;
+        }
+      )
+      console.log("1");
+    }
+
+    else if (this.select_by == "time") {
+      this.chosen = this.instance.sort((a, b) => (a.time.minutes + "" > b.time.minutes + "") ? 1 : ((b.time.minutes + "" > a.time.minutes + "") ? -1 : 0));
+    }
+      // this.chosen = this.instance.sort(
+      //   // (a, b) => { return a.time.hours - b.time.hours }
+      //   // (a, b) => parseFloat(a.time.hours + "") - parseFloat(b.time.hours + "")
+      //   function compare(a, b) {
+      //     if (a.time.hours < b.time.hours)
+      //       return -1;
+      //     if (a.time.hours > b.time.hours)
+      //       return 1;
+      //     return 0;
+      //   }
+      // )
+      // console.log("2");
+
+    else
+      this.chosen = this.instance.sort((a, b) => (a.text[0] > b.text[0]) ? 1 : ((b.text[0] > a.text[0]) ? -1 : 0));
+  }
 }
+
+
