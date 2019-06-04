c06c03f851a3a77411503f08d58eea6b36624efe
lichaytiram <lichaytiram@gmail.com>
2019-06-04 12:12:22 +0300

    web

--- Project/src/app/customer/customer.component.ts
+++ Project/src/app/customer/customer.component.ts
@@ -20,7 +20,6 @@ export class CustomerComponent implements OnInit {
   public token: number;
   public id: number;
 
-  
   private userName: string;
   private password: string;
   private firstName: string;
