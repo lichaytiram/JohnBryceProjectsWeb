9f393c62f4fc897a0ca191edc0377ba31b3419ce
lichaytiram <lichaytiram@gmail.com>
2019-08-03 19:42:35 +0300

    web

--- javaScript/learnJavaScript.js
+++ javaScript/learnJavaScript.js
@@ -5,9 +5,9 @@ function f1() {
 
     this.x = 'hiX';
 
-this.info = function(){
-    return this.x;
-}
+    this.info = function () {
+        return this.x;
+    }
 
 }
 
@@ -17,4 +17,13 @@ let x2 = new f1();
 
 // console.log(x1.x);
 console.log(x2.info);
+
+function show() {
+    if (1 == 1)
+        var a = 5;
+
+    document.write(a);
+}
+
+show();
 
