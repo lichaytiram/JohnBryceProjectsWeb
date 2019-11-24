a72cf86b52cf474cf6da01fa45ba62c125eb46a1
lichaytiram <lichaytiram@gmail.com>
2019-11-24 18:12:51 +0200

    web

---
+++ Practice/try9.js
@@ -0,0 +1,17 @@
+
+// const v = ["name1","name2"];
+// console.log(v);
+// const v2 = [...v]
+// console.log(v2);
+// v2.reverse();
+// console.log(v);
+// console.log(v2);
+
+const v = { name: "value" };
+console.log(v);
+const v2 = { ...v };
+console.log(v);
+console.log(v2);
+v2.name2 = "value2";
+console.log(v);
+console.log(v2);
