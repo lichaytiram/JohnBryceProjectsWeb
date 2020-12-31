05b9148e35b81cccbc7580138b11e140319ef632
lichaytiram <lichaytiram@gmail.com>
2020-12-31 12:16:31 +0200

    hm

---
+++ HTML/hm.js
@@ -0,0 +1,53 @@
+var readlineSync = require('readline-sync')
+// var credit = 0;
+// var removeCredit = 0;
+ 
+// menu = [];
+// menu[0] = "Purchase a product";
+// menu[1] = "View your credit";
+// menu[2] = "Add credit";
+// menu[3] = "Retrieve a refund";
+
+// index = readlineSync.keyInSelect(menu, 'Please choose your option');
+
+
+// function writeValue(number) {
+
+//     if (number >= 0 && number <= 9 && Number.isInteger(number)) {
+
+//         switch (number) {
+//             case 1:
+//                 console.log("one");
+//                 break;
+//             case 2:
+//                 console.log("two");
+//                 break;
+//             case 3:
+//                 console.log("three");
+//                 break;
+//             case 4:
+//                 console.log("four");
+//                 break;
+//             case 5:
+//                 console.log("five");
+//                 break;
+//             case 6:
+//                 console.log("six");
+//                 break;
+//             case 7:
+//                 console.log("seven");
+//                 break;
+//             case 8:
+//                 console.log("eight");
+//                 break;
+//             case 9:
+//                 console.log("nine");
+//                 break;
+//         }
+//         return;
+
+//     }
+//     console.log("Out of range");
+// }
+
+// writeValue("5");
